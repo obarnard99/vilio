f625687a66997dbdf71e85baabf3090315c7c3c9
Ollie Barnard <obarnard99@gmail.com>
2021-02-10 20:04:10 +0000

    fix(data): Fix bug causing crash when there is a newline at end of jsonl

--- fts_tsv/hm_pretrain_data_tsv.py
+++ fts_tsv/hm_pretrain_data_tsv.py
@@ -57,7 +57,7 @@ class LXMERTTorchDataset(Dataset):
         for split in self.splits:
             path = os.path.join("data/", f"{split}.jsonl")
             self.raw_data.extend(
-                    [json.loads(jline) for jline in open(path, "r").read().split('\n')]
+                    [json.loads(jline) for jline in open(path, "r").read().split('\n') if jline != ""]
             )
         print("Load %d data from split(s) %s." % (len(self.raw_data), self.name))
 
