3b3c09ea276609ba13c8306f0cc7618083d329d6
Ollie Barnard <obarnard99@gmail.com>
2021-03-08 13:53:45 +0000

    chore(models): Restructure repo

--- hm.py
+++ hm.py
@@ -16,11 +16,11 @@ else:
 from src.vilio.transformers.optimization import AdamW, get_linear_schedule_with_warmup
 from utils.pandas_scripts import clean_data
 
-from entryU import ModelU
-from entryX import ModelX
-from entryV import ModelV
-from entryD import ModelD
-from entryO import ModelO
+from entry.entryU import ModelU
+from entry.entryX import ModelX
+from entry.entryV import ModelV
+from entry.entryD import ModelD
+from entry.entryO import ModelO
 
 # Two different SWA Methods - https://pytorch.org/blog/pytorch-1.6-now-includes-stochastic-weight-averaging/
 if args.swa:
