f8d69d9f4bdb641d2833a38166d7d13da5aca2e4
Ollie Barnard <obarnard99@gmail.com>
2021-04-02 17:30:53 +0100

    feat(features): update uniter script for small number of features

--- bash/training/hm_U.sh
+++ bash/training/hm_U.sh
@@ -122,3 +122,122 @@ python hm.py \
            --exp U36c \
            --topk $topk \
 
+
+# 5 Feats, Seed 11
+python hm.py \
+           --seed 11 \
+           --model U \
+           --train train \
+           --valid dev_seen \
+           --test dev_seen \
+           --lr 1e-5 \
+           --batchSize 8 \
+           --tr bert-large-cased \
+           --epochs 5 \
+           --tsv \
+           --num_features 5 \
+           --features ./data/features/tsv_clean/hm_vgattr55_clean.tsv \
+           --loadpre ./data/models/uniter-large.pt \
+           --num_pos 6 \
+           --contrib \
+           --exp U36c \
+           --topk $topk \
+
+python hm.py \
+           --seed 11 \
+           --model U \
+           --train traindev \
+           --valid dev_seen \
+           --test test_seen,test_unseen \
+           --lr 1e-5 \
+           --batchSize 8 \
+           --tr bert-large-cased \
+           --epochs 5 \
+           --tsv \
+           --num_features 5 \
+           --features ./data/features/tsv_clean/hm_vgattr55_clean.tsv \
+           --loadpre ./data/models/uniter-large.pt \
+           --num_pos 6 \
+           --contrib \
+           --exp U36c \
+           --topk $topk \
+
+
+# 10 Feats, Seed 97
+python hm.py \
+           --seed 97 \
+           --model U \
+           --train train \
+           --valid dev_seen \
+           --test dev_seen \
+           --lr 1e-5 \
+           --batchSize 8 \
+           --tr bert-large-cased \
+           --epochs 5 \
+           --tsv \
+           --num_features 10 \
+           --features ./data/features/tsv_clean/hm_vgattr1010_clean.tsv \
+           --loadpre ./data/models/uniter-large.pt \
+           --num_pos 6 \
+           --contrib \
+           --exp U36c \
+           --topk $topk \
+
+python hm.py \
+           --seed 97 \
+           --model U \
+           --train traindev \
+           --valid dev_seen \
+           --test test_seen,test_unseen \
+           --lr 1e-5 \
+           --batchSize 8 \
+           --tr bert-large-cased \
+           --epochs 5 \
+           --tsv \
+           --num_features 10 \
+           --features ./data/features/tsv_clean/hm_vgattr1010_clean.tsv \
+           --loadpre ./data/models/uniter-large.pt \
+           --num_pos 6 \
+           --contrib \
+           --exp U36c \
+           --topk $topk \
+
+
+# 15 Feats, Seed 142
+python hm.py \
+           --seed 142 \
+           --model U \
+           --train train \
+           --valid dev_seen \
+           --test dev_seen \
+           --lr 1e-5 \
+           --batchSize 8 \
+           --tr bert-large-cased \
+           --epochs 5 \
+           --tsv \
+           --num_features 15 \
+           --features ./data/features/tsv_clean/hm_vgattr1515_clean.tsv \
+           --loadpre ./data/models/uniter-large.pt \
+           --num_pos 6 \
+           --contrib \
+           --exp U36c \
+           --topk $topk \
+
+python hm.py \
+           --seed 142 \
+           --model U \
+           --train traindev \
+           --valid dev_seen \
+           --test test_seen,test_unseen \
+           --lr 1e-5 \
+           --batchSize 8 \
+           --tr bert-large-cased \
+           --epochs 5 \
+           --tsv \
+           --num_features 15 \
+           --features ./data/features/tsv_clean/hm_vgattr1515_clean.tsv \
+           --loadpre ./data/models/uniter-large.pt \
+           --num_pos 6 \
+           --contrib \
+           --exp U36c \
+           --topk $topk \
