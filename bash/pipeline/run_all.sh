9b9e22e46feb4afdf3f972a1589a677ba7d0075b
Ollie Barnard <obarnard99@gmail.com>
2021-04-10 17:59:27 +0100

    feat(hm): Add D and X models

--- bash/pipeline/run_all.sh
+++ bash/pipeline/run_all.sh
@@ -43,11 +43,11 @@ source $CONDA_ROOT_DIR/bin/activate vilio
 cd $ROOT_DIR
 for EXP in "${EXPERIMENTS[@]}"; do
   read MODEL NUM_FEATS FLAGS <<< "$(sed -r 's/^([A-Z])([0-9]+)([a-z]*)/\1 \2 \3 /' <<< $EXP)"
-  if [[ $MODEL == "U" ]]; then
+  if [[ $MODEL == "U" ]] || [[ $MODEL == "D" ]]; then
     # Train Model
     python hm.py \
                --seed $SEED \
-               --model U \
+               --model $MODEL \
                --train train \
                --valid dev_seen \
                --test dev_seen \
@@ -143,6 +143,71 @@ for EXP in "${EXPERIMENTS[@]}"; do
                --loadpre $DATA_DIR/LAST_$EXP.pth \
                --anno_dir $ANNO_DIR \
                --contrib \
+               --exp $EXP \
+               --topk $TOPK
+
+  elif [[ $MODEL == "X" ]]; then
+    # Pretrain Model
+    python pretrain_bertX.py \
+               --seed $SEED \
+               --taskMaskLM \
+               --wordMaskRate 0.15 \
+               --train pretrain \
+               --tsv \
+               --llayers 12 \
+               --rlayers 2 \
+               --xlayers 5 \
+               --batchSize 16 \
+               --lr 0.5e-5 \
+               --epochs 8 \
+               --num_features $NUM_FEATS \
+               --features $FEATURE_DIR/tsv/"$NUM_FEATS""$FLAGS".tsv \
+               --loadpre $MODEL_DIR/Epoch18_LXRT.pth \
+               --anno_dir $ANNO_DIR \
+               --topk $TOPK
+
+    # Train Model
+    python hm.py \
+               --seed $SEED \
+               --model X \
+               --train train \
+               --valid dev_seen \
+               --test dev_seen \
+               --lr 1e-5 \
+               --batchSize 8 \
+               --tr bert-base-uncased \
+               --epochs 5 \
+               --tsv \
+               --llayers 12 \
+               --rlayers 2 \
+               --xlayers 5 \
+               --num_features $NUM_FEATS \
+               --loadpre $DATA_DIR/LAST_$EXP.pth \
+               --anno_dir $ANNO_DIR \
+               --swa \
+               --exp $EXP \
+               --topk $TOPK
+
+    # Inference
+    python hm.py \
+               --seed $SEED \
+               --model X \
+               --train traindev \
+               --valid dev_seen \
+               --test test_seen,test_unseen \
+               --lr 1e-5 \
+               --batchSize 8 \
+               --tr bert-base-uncased \
+               --epochs 5 \
+               --tsv \
+               --llayers 12 \
+               --rlayers 2 \
+               --xlayers 5 \
+               --num_features $NUM_FEATS \
+               --features $FEATURE_DIR/tsv/"$NUM_FEATS""$FLAGS".tsv \
+               --loadpre $DATA_DIR/LAST_$EXP.pth \
+               --anno_dir $ANNO_DIR \
+               --swa \
                --exp $EXP \
                --topk $TOPK
   fi
