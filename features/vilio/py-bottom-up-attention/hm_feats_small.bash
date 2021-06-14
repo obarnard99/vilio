dcfdf33f0d667fc4e453ad38caeb89660080e925
Oliver Barnard <ojrb2@cam.ac.uk>
2021-06-14 21:13:46 +0100

    feat(feats): Feature extraction utils

--- features/vilio/py-bottom-up-attention/hm_feats_small.bash
+++ features/vilio/py-bottom-up-attention/hm_feats_small.bash
@@ -2,12 +2,13 @@
 DATA_DIR="/home/miproj/4thyr.oct2020/ojrb2/vilio/data/features"
 SPLIT=img_clean
 
+
+python detectron2_mscoco_proposal_maxnms.py --batchsize 4 --split $SPLIT --weight vg \
+--minboxes 36 --maxboxes 36 --dataroot $DATA_DIR
+
+python detectron2_mscoco_proposal_maxnms.py --batchsize 4 --split $SPLIT --weight vg \
+--minboxes 20 --maxboxes 20 --dataroot $DATA_DIR
+
 # Attr feats:
 python detectron2_mscoco_proposal_maxnms.py --batchsize 4 --split $SPLIT --weight vgattr \
---minboxes 5 --maxboxes 5 --dataroot $DATA_DIR
-
-python detectron2_mscoco_proposal_maxnms.py --batchsize 4 --split $SPLIT --weight vgattr \
---minboxes 10 --maxboxes 10 --dataroot $DATA_DIR
-
-python detectron2_mscoco_proposal_maxnms.py --batchsize 4 --split $SPLIT --weight vgattr \
---minboxes 15 --maxboxes 15 --dataroot $DATA_DIR
+--minboxes 20 --maxboxes 20 --dataroot $DATA_DIR
